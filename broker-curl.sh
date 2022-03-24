dfd1581b2ee7ee566901018183c48f02cbdb389c
Ishan Khare <me@ishankhare.dev>
2022-03-24 14:26:14 +0530

    add curl for triggering initial cloud event
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

--- broker-curl.sh
+++ broker-curl.sh
@@ -1,8 +1,12 @@
-curl -v "http://el-gke-provision-listener.default.svc.cluster.local:8080" \
+# curl -v "http://broker-ingress.knative-eventing.svc.cluster.local/default/default" \
+curl --insecure -v "https://broker.ishankhare.dev/default/default" \
 -X POST \
--H "Ce-Id: say-hello-goodbye" \
+-H "Ce-Id: create-gke-cluster" \
 -H "Ce-Specversion: 1.0" \
--H "Ce-Type: greeting" \
--H "Ce-Source: sendoff" \
+-H "Ce-Type: dev.cd.taskrun.started.v1" \
+-H "Ce-Source: curl" \
 -H "Content-Type: application/json" \
--d '{"path": "workspace/source/config/crossplane/resources/", "git_source_name": "cluster-git"}'
+-d '{
+	"path": "workspace/source/config/crossplane/resources/",
+	"git_source_name": "cluster-git"
+}'

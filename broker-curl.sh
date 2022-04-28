cc671e1f8f0cff3877789f15665394bb6ea0c62f
Ishan Khare <me@ishankhare.dev>
2022-04-28 20:02:14 +0530

    modify broker curl
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

--- broker-curl.sh
+++ broker-curl.sh
@@ -1,4 +1,4 @@
-# curl -v "http://broker-ingress.knative-eventing.svc.cluster.local/default/default" \
+# curl --insecure -v "http://broker-ingress.knative-eventing.svc.cluster.local/default/default" \
 curl --insecure -v "https://broker.ishankhare.dev/default/default" \
 -X POST \
 -H "Ce-Id: create-gke-cluster" \
@@ -6,6 +6,7 @@ curl --insecure -v "https://broker.ishankhare.dev/default/default" \
 -H "Ce-Type: dev.cd.taskrun.started.v1" \
 -H "Ce-Source: curl" \
 -H "Content-Type: application/json" \
+-H "Host: broker.ishankhare.dev" \
 -d '{
 	"path": "workspace/source/config/crossplane/resources/",
 	"git_source_name": "cluster-git"

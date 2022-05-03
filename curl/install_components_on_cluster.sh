ba0a9c073dd37e56edb34ca2a47f8be24e26e41a
Ishan Khare <me@ishankhare.dev>
2022-05-03 22:52:23 +0530

    add curls for triggering individual pipelines
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

---
+++ curl/install_components_on_cluster.sh
@@ -0,0 +1,13 @@
+# curl --insecure -v "http://broker-ingress.knative-eventing.svc.cluster.local/default/default" \
+curl --insecure -v "https://broker.ishankhare.dev/default/default" \
+-X POST \
+-H "Ce-Id: create-gke-cluster" \
+-H "Ce-Specversion: 1.0" \
+-H "Ce-Type: dev.cd.environment.created.v1" \
+-H "Ce-Source: curl" \
+-H "Content-Type: application/json" \
+-H "Host: broker.ishankhare.dev" \
+-d '{
+	"path": "workspace/source/config/crossplane/resources/",
+	"git_source_name": "cluster-git"
+}'

f745097bc741b413fff098d5ad640b1d6f69392b
Ishan Khare <me@ishankhare.dev>
2022-05-05 18:28:16 +0530

    add deploy ksvc events controller to the GKE cluster
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

--- curl/install_components_on_cluster.sh
+++ curl/install_components_on_cluster.sh
@@ -1,13 +1,14 @@
 # curl --insecure -v "http://broker-ingress.knative-eventing.svc.cluster.local/default/default" \
 curl --insecure -v "https://broker.ishankhare.dev/default/default" \
 -X POST \
--H "Ce-Id: create-gke-cluster" \
+-H "Ce-Id: setup-gke-cluster" \
 -H "Ce-Specversion: 1.0" \
 -H "Ce-Type: dev.cd.environment.created.v1" \
 -H "Ce-Source: curl" \
 -H "Content-Type: application/json" \
 -H "Host: broker.ishankhare.dev" \
 -d '{
-	"path": "workspace/source/config/crossplane/resources/",
-	"git_source_name": "cluster-git"
+	"path": "workspace/source/config/",
+	"git_source_name": "cluster-git",
+	"docker_repo": "ishankhare07"
 }'

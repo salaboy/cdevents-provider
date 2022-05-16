bf3db86b7082909c7666a8fc66046e6d8b3cfb2a
Ishan Khare <me@ishankhare.dev>
2022-05-16 14:22:36 +0200

    add magic dns configuration
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

--- curl/ksvc-deployed-success.sh
+++ curl/ksvc-deployed-success.sh
@@ -1,10 +1,11 @@
 # curl --insecure -v "http://broker-ingress.knative-eventing.svc.cluster.local/default/default" \
+# curl --insecure -v http://sockeye.default.34.100.170.210.sslip.io \
 curl --insecure -v "https://broker.ishankhare.dev/default/default" \
 -X POST \
 -H "Ce-Id: create-gke-cluster" \
 -H "Ce-Specversion: 1.0" \
 -H "Ce-Type: cd.service.deployed.v1" \
 -H "Ce-Source: default/hello" \
--H "Content-Type: application/json" \
 -H "Host: broker.ishankhare.dev" \
+-H "Content-Type: application/json" \
 -d '{}'
